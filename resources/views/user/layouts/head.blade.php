c5b00a1e84e0341ddacc318c39d2bc18520475f6
Geovanny Mena <geovannymenavc@gmail.com>
2024-11-21 16:40:02 -0500

    UPDATE HEADER DASHBOARD

--- resources/views/user/layouts/head.blade.php
+++ resources/views/user/layouts/head.blade.php
@@ -9,6 +9,7 @@
     <title>Shop Eiteck | DASHBOARD</title>
   
     <!-- Custom fonts for this template-->
+    <link rel="icon" type="image/png" href="{{ asset('images/cart.png') }}">
     <link href="{{asset('backend/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   
